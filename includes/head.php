887fa6f037796c687ef08f67365dfc48727b87be
Agustin <aguscontreras@outlook.com.ar>
2020-06-12 00:33:15 -0300

    Separado layout y Style

---
+++ includes/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Sistema de tickets</title>
+</head>
+
+<body class="b-body">
---
+++ includes/header_nav.php
@@ -0,0 +1,99 @@
+<header class="b-header">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="b-account-menu-container">
+                    <a href="#" class="b-link b-account-menu">
+                        <span class="b-account-menu__e-text">
+                            Agustín Contreras
+                        </span>
+                        <span class="material-icons b-account-menu__e-icon b-account-menu__e-icon--active">
+                            expand_more
+                        </span>
+                    </a>
+                    <div class="b-dropdown b-dropdown--right">
+                        <div class="b-dropdown__e-container">
+                            <ul class="b-ul b-dropdown-list">
+                                <li class="b-li b-dropdown-list__e-item">
+                                    <a href="#" class="b-link b-dropdown-list__e-link b-dropdown-list__e-link--is-icons">
+                                        <span class="b-dropdown-list__e-text">
+                                            Mi perfil
+                                        </span>
+                                        <span class="material-icons b-dropdown-list__e-icon">
+                                            face
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="b-li b-dropdown-list__e-item">
+                                    <a href="#" class="b-link b-dropdown-list__e-link b-dropdown-list__e-link--is-icons">
+                                        <span class="b-dropdown-list__e-text">
+                                            Desconectarme
+                                        </span>
+                                        <span class="material-icons b-dropdown-list__e-icon">
+                                            exit_to_app
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="b-logo-container">
+                    <img src="" alt="logo" class="b-logo-container__e-logo">
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<nav class="d-md-none">
+    <div class="container-fluid">
+        <div class="row b-nav-mobile">
+            <div class="col">
+                <div class="b-nav-mobile-header">
+                    <span class="b-nav-mobile-header__e-text">
+                        Tickets
+                    </span>
+                    <span class="material-icons b-nav-mobile-header__e-icon">
+                        menu
+                    </span>
+                </div>
+                <div class="b-nav-mobile-container">
+                    <ul class="b-ul b-nav-mobile-list">
+                        <li class="b-nav-mobile-list__e-nav-item">
+                            <a href="#" class="b-link b-nav-mobile-list__e-nav-link">Tickets</a>
+                        </li>
+                        <li class="b-nav-mobile-list__e-nav-item">
+                            <a href="#" class="b-link b-nav-mobile-list__e-nav-link">Solicitudes</a>
+                        </li>
+                        <li class="b-nav-mobile-list__e-nav-item">
+                            <a href="#" class="b-link b-nav-mobile-list__e-nav-link">Agentes</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<nav class="b-nav d-none d-md-block">
+    <div class="container">
+        <div class="row b-nav">
+            <div class="col">
+                <div class="b-nav-container">
+                    <ul class="b-ul b-nav-list">
+                        <li class="b-nav-list__e-nav-item">
+                            <a href="#" class="b-link b-nav-list__e-nav-link b-nav-list__e-nav-link--active">Tickets</a>
+                        </li>
+                        <li class="b-nav-list__e-nav-item">
+                            <a href="#" class="b-link b-nav-list__e-nav-link">Solicitudes</a>
+                        </li>
+                        <li class="b-nav-list__e-nav-item">
+                            <a href="#" class="b-link b-nav-list__e-nav-link">Agentes</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
